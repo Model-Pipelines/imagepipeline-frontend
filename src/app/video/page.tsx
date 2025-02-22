7601fe403b302b774eb94960f40fbb2fdbc9eca2
Virender Prasad <virender288@gmail.com>
2025-02-22 20:24:21 +0530

    made pages for adio and video

---
+++ src/app/video/page.tsx
@@ -0,0 +1,11 @@
+import ComingSoonPage from "@/components/ComingSoon/ComingSoonPage";
+
+const Page = () => {
+    return (
+        <>
+            <ComingSoonPage />
+        </>
+    );
+};
+
+export default Page;
